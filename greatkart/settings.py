6d9db6438896e747d8cf5eccc30676a252984b9a
Amir Bayjanov <howareyouxan@gmail.com>
2021-09-26 15:40:56 +0500

    django authentication

--- greatkart/settings.py
+++ greatkart/settings.py
@@ -134,3 +134,16 @@ STATICFILES_DIRS = [
 # media files configuration
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
+
+# django messages
+from django.contrib.messages import constants as messages
+MESSAGE_TAGS = {
+    messages.ERROR: 'danger',
+}
+
+# SMTP configuration
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_HOST_USER = 'howareyouxan@gmail.com'
+EMAIL_HOST_PASSWORD = '712007amirxan'
+EMAIL_USE_TLS = True
